a10141c25213c97540e1b416b9fc4d0f028f26c3
François Guillot <francoisguillot73@gmail.com>
2015-07-26 23:59:50 +0200

    go to version 1.0.5

--- src/main/java/org/facewindu/breastfeedingbuddy/BreastFeedingBuddyFX.java
+++ src/main/java/org/facewindu/breastfeedingbuddy/BreastFeedingBuddyFX.java
@@ -55,7 +55,7 @@ public class BreastFeedingBuddyFX extends Application {
 	private Bounds bounds;
 	private final static int MARGIN = 36;
 	private static final String TITLE = "BreastFeedingBuddyFX";
-	public static final String VERSION = "1.0.4";
+	public static final String VERSION = "1.0.5";
 	private StackPane root;
 
 	@Override
